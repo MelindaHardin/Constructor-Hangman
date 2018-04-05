9c1f4419f8e120d16faa22c8c5286d658a0b0ac9
Melinda Hardin <melindawad@gmail.com>
2018-04-04 20:45:21 -0500

    notes

--- letter.js
+++ letter.js
@@ -1,8 +1,12 @@
-//This constructor should be able to either display an underlying character or a blank placeholder (such as an underscore), depending on whether or not the user has guessed the letter. That means the constructor should define:
-
+//This constructor should be able to either display an underlying character or a blank placeholder (such as an underscore)depending on whether or not the user has guessed the letter. That means the constructor should define:
 var Letter = function (character) {
+  //A string value to store the underlying character for the letter
   this.character = character;
+
+  //A boolean value that stores whether that letter has been guessed yet
   this.underscore = false;
+
+  //A function that returns the underlying character if the letter has been guessed, or a placeholder (like an underscore) if the letter has not been guessed
   this.guess = function () {
     if (this.underscore === false) {
       return "_";
@@ -10,6 +14,8 @@ var Letter = function (character) {
       return this.character;
     }
   };
+
+  //A function that takes a character as an argument and checks it against the underlying character, updating the stored boolean value to true if it was guessed correctly
   this.check = function (letter) {
     if (this.letter === character) {
       this.underscore = true;
@@ -18,6 +24,8 @@ var Letter = function (character) {
       return false;
     }
   };
+  l
 };
-
+//Letter.js should not require any other files.
+//Word.js should only require Letter.js
 module.export = Letter;
